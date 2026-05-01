2317318a1198babcb9cd52d70826aa8b60411dd4
Juan_d <juanvasquez0728@gmail.com>
2026-05-01 10:12:09 -0500

    Cambios

--- ENTIDADES/Properties/AssemblyInfo.cs
+++ ENTIDADES/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("METODOS")]
+[assembly: AssemblyTitle("ENTIDADES")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("METODOS")]
+[assembly: AssemblyProduct("ENTIDADES")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("9ed35e01-fb7b-4f00-8a58-2a26560dbb39")]
+[assembly: Guid("e276fb04-2706-469d-a57c-628b4bba81bc")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
