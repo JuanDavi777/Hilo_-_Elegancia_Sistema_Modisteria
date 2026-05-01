c714334a52b1fb04fc0bc71e99fbae2965239ec5
Juan_d <juanvasquez0728@gmail.com>
2026-05-01 11:28:44 -0500

    se agrego numero como atributo

--- ENTIDAD/OrdenTrabajo.cs
+++ ENTIDAD/OrdenTrabajo.cs
@@ -8,6 +8,7 @@ namespace ENTIDAD
 {
     public class OrdenTrabajo
     {
+        public int numero;
         public int IdOrden;
         public int IdPedido;
         public int AsignadoA; // Id del usuario (costurero)
